8fe4e0fe3c3201068c303660ba528bdbe687c8bf
ali <117563531+alidxpm@users.noreply.github.com>
2023-08-22 15:59:10 +0800

    Add files via upload

---
+++ app/component/newshdu.js
@@ -0,0 +1,11 @@
+import styles from '../styles/newshdu.module.css'
+export default function Newshdu(){
+    return(
+        <div className={styles.content}>
+            <div className={styles.content1}>
+            </div>
+            <div className={styles.content2}>
+            </div>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,17 +1,27 @@
 import Image from 'next/image'
-import Header from '@/components/header'
-import Teacher from '@/components/teacher'
-import Footer from '@/components/footer'
-import styles from '@/styles/components/page.module.css'
-import './globals.css'
-import Show from '@/components/show'
-export default function Home () {
+import  styles  from './styles/newspart.module.css'
+import titleicon from './public/images/newstitleicon.png'
+import ParentComponent from './component/parentcomponent'
+
+
+export default function Newspart() {
   return (
-    <div className={styles.container}>
-      <Header />
-      <Show />
-      <Teacher />
-      <Footer />
-    </div>
+    <>
+      <div className={styles.bgc}> 
+        <div className={styles.ellibgc}>
+        </div>   
+        <div className={styles.news}>
+              <div className={styles.title}>
+                <Image src={titleicon} className={styles.titleicon}></Image>
+                <span className={styles.word}>新闻动态</span>
+                <span className={styles.Engword}>/News Trends</span>
+              </div>
+              <div className={styles.content}>
+              <ParentComponent></ParentComponent>     
+              </div>
+        </div>   
+      </div>
+    </>
+   
   )
 }
